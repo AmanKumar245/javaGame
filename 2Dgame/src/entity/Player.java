459be2431bab43d568f7c352a18e38959201d2e7
Aman Kumar <letscode2457@gmail.com>
2025-05-14 01:42:17 +0530

    Add initial mapLayout

---
+++ 2Dgame/src/entity/Player.java
@@ -0,0 +1,121 @@
+package entity;
+
+import main.GamePanel;
+import main.keyHandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Player extends entity{
+
+    GamePanel gp;
+    keyHandler keyH;
+
+    public Player(GamePanel gp, keyHandler keyH){
+        this.gp = gp;
+        this.keyH = keyH;
+        setDefaultValues();
+        getPlayerImage();
+    }
+    public void setDefaultValues(){
+        X = 100;
+        Y = 100;
+        Speed = 4;
+        direction = "down";
+
+    }
+    public void getPlayerImage(){
+        try{
+            up1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_1.png")));
+            up2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_2.png")));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void update(){
+        if (keyH.upPressed || keyH.downPressed || keyH.leftPressed|| keyH.rightPressed){
+
+            if (keyH.upPressed){
+                direction = "up";
+                Y -= Speed;
+
+            } else if (keyH.downPressed) {
+                direction = "down";
+                Y += Speed;
+            } else if (keyH.leftPressed) {
+                direction = "left";
+                X -= Speed;
+
+            } else if (keyH.rightPressed) {
+                direction = "right";
+                X +=Speed;
+
+            }
+            spriteCounter++;
+            if(spriteCounter >12){
+                if(spriteNum ==1){
+                    spriteNum=2;
+                } else if (spriteNum ==2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+        }
+    }
+
+    public void draw(Graphics2D g2){
+        //g2.setColor(Color.white);
+        //g2.fillRect(X, Y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+
+        switch(direction){
+            case "up":
+                if(spriteNum ==1){
+                    image = up1;
+                }
+                if(spriteNum ==2){
+                    image = up2;
+                }
+                break;
+            case "down":
+                if(spriteNum ==1){
+                    image = down1;
+                }
+                if(spriteNum ==2){
+                    image = down2;
+                }
+                break;
+            case "right":
+                if(spriteNum ==1){
+                    image = right1;
+                }
+                if(spriteNum ==2){
+                    image = right2;
+                }
+                break;
+            case "left":
+                if(spriteNum ==1){
+                    image = left1;
+                }
+                if(spriteNum ==2){
+                    image = left2;
+                }
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + direction);
+        }
+        g2.drawImage(image , X, Y, gp.tileSize, gp.tileSize, null);
+
+    }
+}
---
+++ 2Dgame/src/entity/entity.java
@@ -0,0 +1,16 @@
+package entity;
+
+import java.awt.image.BufferedImage;
+
+public class entity {
+    public int X, Y;
+    public int Speed;
+
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public String direction;
+    public int  spriteCounter = 0;
+    public int spriteNum = 1;
+
+
+
+}
